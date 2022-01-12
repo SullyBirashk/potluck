e2f0a6bd934e445b6aa50355ebb5c0d244dca564
Sulaiman Birashk <sully.birashk@gmail.com>
2022-01-11 17:48:28 -0800

    Half of Iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -19,4 +19,27 @@ class Potluck
     end
   end
 
+  def menu
+    # @category = []
+    # @collector = []
+    #
+    # hash = Hash.new {0}
+    #   @dishes.each do |dish|
+    #     @category << dish.category
+    #   end
+    #   uniq_categories = @category.uniq
+    #
+    #   hash = Hash.new{ |hash,key| hash[key] = []}
+    #     uniq_categories.each do |category|
+    #       x = @dishes.each do |dish|
+    #         if dish.category == category
+    #           @collector << dish.name
+    #         end
+    #       end
+    #       hash[category] = x
+    #     end
+    #     return hash
+  end
+
+
 end
--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Potluck do
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
     @summer_pizza = Dish.new("Summer Pizza", :appetizer)
     @roast_pork = Dish.new("Roast Pork", :entre)
-    @candy_salad = Dish.new("Candy Salad", :dessert)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
+
   end
 
   it 'exist' do
@@ -50,5 +52,15 @@ RSpec.describe Potluck do
     expect(@potluck.get_all_from_category(:appetizer).first.name).to eq('Couscous Salad')
   end
 
+  it 'can show you the menu' do
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+# binding.pry
+    expect(@potluck.menu).to eq({:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],:entres=>["Cocktail Meatballs", "Roast Pork"],:desserts=>["Candy Salad"]})
+  end
 
 end
